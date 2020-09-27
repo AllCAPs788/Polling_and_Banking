a6c656001b6a07282b943d92457171e84f8552a0
ALLCAPs788 <capspfifer@gmail.com>
2020-09-27 18:33:13 -0400

    Sun 6:33

--- bank.py
+++ bank.py
@@ -1,24 +1,27 @@
-import statistics
-
 import os
 import csv
 #refer to wrestler functions and how to read/write exercises
 # Path to collect data from the Resources folder
 budget_data = os.path.join('Resources','budget_data.csv')
-# # Method 1: Plain Reading of CSV files
+
  # Split the data on commas
 with open(budget_data) as csvfile:    
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    header = next(csvreader)
-    print(csvreader)
-#header = nextcsv, skip headers to get to data 
-def bank_stats(budget_data):
-    Date = str(budget_data[0])
-    Profits_Losses = int(budget_data[1])
-    Case_count = 86
     
-    Months = sum(Date)
+    #print(header)
+    header = next(csvreader)
+    for row in csvreader:
+        print(row) # prints the entire row
+        #print(f'First column value: {row[0]}') # prints only first column value for each row 
+
+#header = nextcsv, skip headers to get to data 
+#def bank_stats(budget_data):
+    #Date = str(budget_data[0])
+    #Profits_Losses = int(budget_data[1])
+    #Case_count = 86
+    
+    #Months = sum(Date)
     #PL_average = Profits_Losses/Case_count
    
     #Increase =Profits_Losses.max (whatever function returns max amount)
@@ -31,17 +34,15 @@ def bank_stats(budget_data):
 
     #text_file.close() 
 
-bank_stats  
+#bank_stats  
     
-    #print(Date)
+    
     
 
     
     
-    #months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
     #return float(statistics.mean(Profits_Losses))
     #for row in csvreader:
-#print(print_percentages)
 #Your task is to create a Python script that analyzes the records to calculate each of the following:
 
     
@@ -57,16 +58,6 @@ bank_stats
     
 
 #row_count = sum(1 for row in budget_data.csv)  # fileObject is your csv.reader
-
-#Print(Date)
-
-
- # Loop through the data
-    
-
-        # If the wrestler's name in a row is equal to that which the user input, run the 'print_percentages()' function
-        #if name_to_check == row[0]:
-            #print_percentages(row)
 
   #for printing to Budget_Analysis.txt
 
