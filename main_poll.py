22a3ed770aa39406141803457b04debe509096a6
ALLCAPs788 <capspfifer@gmail.com>
2020-09-28 10:59:17 -0400

    Mon 11am

--- main_poll.py
+++ main_poll.py
@@ -1,4 +1,4 @@
-#Import statistics
+import statistics
 import os
 import csv
 #refer to wrestler functions and how to read/write exercises
@@ -13,18 +13,32 @@ with open(election_data) as csvfile:
     #print(header)
     header = next(csvreader)
     for row in csvreader:
+        
+    def Vote_Stats(election_data):
         print(row[0]) # prints the entire row
         #print(f'First column value: {row[0]}') # prints only first column value for each row 
+        Candidates() = str("Corey","Khan","Li", "O'Tooley")   
         vote = row[0]
-        vote_cast =   # fileObject is your csv.reader
+        vote_cast = #The total number of votes each candidate won print('{}: {}% ({})'.format(key,round((key_votes/total_number_votes * 100),3),key_votes))
         vote_total = sum(vote)
         County() = str(election_data[1]) # A complete list of candidates who received votes
-    
-    #Candidates() = str("Corey","Khan","Li")    
-    # print('{}: {}% ({})'.format(key,round((key_votes/total_number_votes * 100),3),key_votes))
+        winner = 
+        print(Candidates[])
+        print(vote)
+
+        #text_file = open("Output.txt", "w")
+
+        #text_file.write(f"Winner: {winner}" "Candidate votes:{candidate votes" "vote percentage:{vote percentage}")
+            print(f"Stats for {name}")
+    print(f"WIN PERCENT: {str(win_percent)}")
+    print(f"LOSS PERCENT: {str(loss_percent)}")
+    print(f"DRAW PERCENT: {str(draw_percent)}")
+    print(f"{name} is a {type_of_wrestler}")
+        #text_file.close()
+    # 
   # The percentage of votes each candidate won: Candidate_Percent = vote_number/vote_cast
   
-#* The total number of votes each candidate won: vote_cast
+
 # The winner of the election based on popular vote.
 # The total number of votes cast: vote_total = sum
 
@@ -36,20 +50,13 @@ with open(election_data) as csvfile:
 # 
 
 
-# fine the function and have it accept the 'wrestler_data' as its sole parameter
-#def print_percentages(election_data):
-    # For readability, it can help to assign your values to variables with descriptive names
+
     
     
-    #votes = int(election_data[0])
+    
     
     
 
 
-#print(vote_cast)
 
-#text_file = open("Output.txt", "w")
 
-#text_file.write(f"Winner: {winner}" "Candidate votes:{candidate votes" "vote percentage:{vote percentage}")
-
-#text_file.close()
