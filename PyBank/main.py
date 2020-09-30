26203b9929eb6a226466bd28f46fd0bfb0046da9
ALLCAPs788 <capspfifer@gmail.com>
2020-09-30 13:00:15 -0400

    Almost done!

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,5 @@
-import statistics
+from statistics import mean
+
 import os
 import csv
 
@@ -9,44 +10,61 @@ with open(budget_data) as csvfile:
 
 
     header = next(csvreader)
-    first_row = next(csvreader)
+    Month_Counter = 0
+    Month_diff= {}
+    #first_row = next(csvreader)
+    Last_month = None
+    Last_PL = None
+    PL_diff = 0
     for row in csvreader:
 
     #Profits_Losses = []
-        Month_Counter = 1 
-        Profits_Losses = row[1]
-        Profits_Losses.append(first_row)
-        Difference = []
-    
-    
-        Date = row[0] 
-        Profits_Losses = int(row[1])
         Month_Counter = Month_Counter+1 
-        Profits_Losses.append(row[1]) 
-        #Difference = (row[1] - next(row[1])    
-        Difference.append(next(row[1]) - row[1])
-   #look up syntax for specifying list values
-    #Increase = max(Profits_Losses) 
-    #Decrease = min(Profits_Losses)
-    
-    Profits_Total = sum(Profits_Losses) + int(first_row[1])
-    Dif_average = float(statistics.mean(Difference))   
-    
+        Date = row[0] 
+        Profits_Losses = row[1]
+        if Last_month!=None:
+            PL_diff = float(Profits_Losses)-float(Last_PL)  
+            Month_diff[Date]=PL_diff
+        Last_month = Date
+        Last_PL = Profits_Losses
+        #first_row = next(csvreader)
+        #Profits_Losses.append(next(row[1]))
+       # Difference = []
+    print(Month_diff)
     print(Month_Counter)
-    print(Profits_Total)
+        
+        #Profits_Losses = int(row[1])
+        
+        #Profits_Losses.append(row[1]) 
+        #Difference = (row[1] - next(row[1])    
+        #Difference.append(next(row[1]) - row[1])
+   #look up syntax for specifying list values
+    Increase_Month = max(Month_diff)
+    Increase = Month_diff[Increase_Month]
+    #Increase_Month = max(Month_diff,key=Month_diff.get)
+    
+    print(Increase_Month) 
+    print(Increase)
+    
+    #Decrease = min(Month_diff)
+    #print(Decrease)
+    Profits_Total = sum(Profits_Losses) + int(first_row[1])
+    Dif_average = round(mean(Month_diff.values()),2)  
+   
     print(Dif_average)
-    #print(Increase)
+    print(Profits_Total)
+    #print(Increase_Month)
     #print(Decrease)
     
-    Budget_Analysis = os.path.join('Analysis','Budget_Analysis.txt')
+    #Budget_Analysis = os.path.join('Analysis','Budget_Analysis.txt')
     with open(Budget_Analysis, 'w') as results_file:
-    #results_file = open("Budget_Analysis.txt", "w")
-       results_file.write(
+    results_file = open("Budget_Analysis.txt", "w")
+        results_file.write(
             f"There are {Month_Counter} months:\n" 
             f"The net total amount of 'Profit/Losses' is {Profits_Total} for the budget:\n" 
             f"The Profit/Loss average is {Dif_average} for the budget:\n" 
-            f"The largest increase is {Increase} for the budget:\n" 
-            f"The largest decrease is {Decrease} for the budget:")
+            f"The largest increase is {Increase_Month} for the budget:\n" 
+            f"The largest decrease is {Decrease_Month} for the budget:")
     #text_file.close() 
 
 
